2f01d6872b84a68e007e4f5e2da600560439b5db
Samuel <samuel.pisa@tegra.me>
2017-01-14 16:31:44 -0200

    week 2 solution

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -29,10 +29,10 @@ class FunSetSuite extends FunSuite {
   /**
    * Tests are written using the "test" operator and the "assert" method.
    */
-  // test("string take") {
-  //   val message = "hello, world"
-  //   assert(message.take(5) == "hello")
-  // }
+   test("string take") {
+     val message = "hello, world"
+     assert(message.take(5) == "hello")
+   }
 
   /**
    * For ScalaTest tests, there exists a special equality operator "===" that
@@ -43,9 +43,9 @@ class FunSetSuite extends FunSuite {
    * Try it out! Change the values so that the assertion fails, and look at the
    * error message.
    */
-  // test("adding ints") {
-  //   assert(1 + 2 === 3)
-  // }
+   test("adding ints") {
+     assert(1 + 2 === 3)
+   }
 
 
   import FunSets._
@@ -77,6 +77,7 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
   }
 
   /**
@@ -107,6 +108,76 @@ class FunSetSuite extends FunSuite {
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("Intersect contains only 2") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val set2 = union(s2, s4)
+      val s = intersect(set1, set2)
+      assert(contains(s, 2), "Intersect 1")
+      assert(!contains(s, 4), "Intersect 4")
+    }
+  }
+
+  test("filter") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val f = filter(set1, (_ == 1))
+      assert(contains(f, 1), "Filter 1")
+      assert(!contains(f, 2), "Filter 2")
+    }
+  }
+
+  test("diff") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val set2 = union(s4, set1)
+      val s = diff(set2, set1)
+      assert(contains(s, 4), "Diff 4")
+      assert(!contains(s, 1), "Diff 1")
+      assert(!contains(s, 2), "Diff 2")
+    }
+  }
+
+  test("forall") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val set2 = union(s3, s4)
+      val s = union(set1, set2)
+
+      assert(forall(s, _ <= 4), "Forall <= 4")
+      assert(!forall(s, _ < 2), "Forall < 2")
+    }
+  }
+
+  test("exists") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val set2 = union(s3, s4)
+      val s = union(set1, set2)
+
+      assert(exists(s, _ > 2), "Exists > 2")
+      assert(!exists(s, _ > 4), "Exists > 4")
+    }
+  }
+
+  test("map") {
+    new TestSets {
+      val set1 = union(s1, s2)
+      val set2 = union(s3, s4)
+      val s = union(set1, set2)
+      val mapSet = map(s, _ * 5)
+
+      assert(contains(mapSet, 5), "Map 5")
+      assert(contains(mapSet, 10), "Map 10")
+      assert(contains(mapSet, 15), "Map 15")
+      assert(contains(mapSet, 20), "Map 20")
+
+      assert(!contains(mapSet, 1), "Map 1")
+      assert(!contains(mapSet, 2), "Map 2")
+      assert(!contains(mapSet, 3), "Map 3")
     }
   }
 
